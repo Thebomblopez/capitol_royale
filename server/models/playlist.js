597353418742df4b557b444c04de3b910be9d903
Hector Lopez <thebomblopez@gmail.com>
2019-11-16 13:53:45 -0800

    Added Playlist Model

---
+++ server/models/playlist.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const PlaylistSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        unique: true
+    },
+    songs: [],
+    users: [],
+}, { timestamps: true })
+
+mongoose.model("Playlist", PlaylistSchema);
+
